d29c175a70d3f3857127f46ff287009168a8633b
fangsj <fangshijie@keisdom.com>
2019-05-22 15:21:09 +0800

    框架提交

---
+++ src/main/java/org/fangsj/learn/spring/boot/jta/Application.java
@@ -0,0 +1,30 @@
+//*****************************************************************************
+//
+// File Name       :  org.fangsj.learn.spring.boot.jta
+// Created Date    :  2019-05-22
+// Created By      :  fangshijie
+// Last Changed By :  fangshijie
+// Last Changed On :  2019-05-22
+// Description     :  //TODO To fill in a brief description of the purpose of
+//                    this class.
+//
+// Keisdom Pte Ltd.  Copyright (c) 2019.  All Rights Reserved.
+//
+//*****************************************************************************
+package org.fangsj.learn.spring.boot.jta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 说明
+ *
+ * @author fangshijie
+ * @date 2019-05-22 14:52
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
